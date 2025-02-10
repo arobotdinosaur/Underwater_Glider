3112aaf73de0187e8ca81c1755abb517e67b75b6
arobotdinosaur <160431417+arobotdinosaur@users.noreply.github.com>
2025-02-10 11:54:12 -0800

    firmware

---
+++ Hydrosub_Firmware/Hydrosub_Firmware.ino
@@ -0,0 +1,15 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM6DS.h>
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+lsm.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
