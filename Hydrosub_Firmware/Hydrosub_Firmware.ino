1c36a2e8eda262f966042b49c919f7cf9542a18f
arobotdinosaur <160431417+arobotdinosaur@users.noreply.github.com>
2025-02-12 22:11:37 -0800

    can write to thruster now

--- Hydrosub_Firmware/Hydrosub_Firmware.ino
+++ Hydrosub_Firmware/Hydrosub_Firmware.ino
@@ -1,16 +1,20 @@
+//for this to flash, you need to change the processor to ATmega328P (old bootloader) in the tools menu
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM6DS.h>
 #include <Servo.h>
 Servo thruster;
 void setup() {
-  // put your setup code here, to run once:
 Serial.begin(9600);
 //lsm.begin();
-thruster.attach(A3);
+thruster.attach(9); //pin d9 on arduino
+
+thruster.writeMicroseconds(1500);  // Neutral position (motor stopped), needed for esc initialization
+delay(2000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-thruster.write(12);
+thruster.writeMicroseconds(1510);
+delay(1000);
 }
