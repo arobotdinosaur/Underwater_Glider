9e00893b0d4a0fbef91758d3f1b7fe421827c39f
arobotdinosaur <160431417+arobotdinosaur@users.noreply.github.com>
2025-02-12 16:04:53 -0800

    doesnt work yet

--- Hydrosub_Firmware/Hydrosub_Firmware.ino
+++ Hydrosub_Firmware/Hydrosub_Firmware.ino
@@ -1,15 +1,16 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM6DS.h>
-
+#include <Servo.h>
+Servo thruster;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-lsm.begin();
-
+//lsm.begin();
+thruster.attach(A3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+thruster.write(12);
 }
